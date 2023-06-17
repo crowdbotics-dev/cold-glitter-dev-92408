17b3ac466de9c15c02206bbdcab50cc24d53ed8f
Crowdbotics <engineering@crowdbotics.com>
2023-06-17 06:08:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cold-glitter-dev-92408",
+  url: "https://cold-glitter-dev-92408.botics.co",
+  api: "https://cold-glitter-dev-92408.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
